04317c8ba27b84d156a71f52bc8cf602d7c8e556
Alex Zero <joseph@marsden.space>
2021-06-27 18:37:30 +0100

    refactor: seperate tpm and tpm2 profiles

--- profiles/core/security/tpm/default.nix
+++ profiles/core/security/tpm/default.nix
@@ -1,11 +1,6 @@
 { pkgs, ... }:
 {
-    security.tpm2 = {
-        enable = true;
+    services.tcsd.enable = true;
 
-        abrmd.enable = true;
-        pkcs11.enable = true;
-    };
-
-    environment.systemPackages = with pkgs; [ tpm2-tools ];
+    environment.systemPackages = with pkgs; [ tpm-tools ];
 }
---
+++ profiles/core/security/tpm2/default.nix
@@ -0,0 +1,11 @@
+{ pkgs, ... }:
+{
+    security.tpm2 = {
+        enable = true;
+
+        abrmd.enable = true;
+        pkcs11.enable = true;
+    };
+
+    environment.systemPackages = with pkgs; [ tpm2-tools ];
+}
