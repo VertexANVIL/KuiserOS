29c9f96d55213de2954fd9bfb8cfd0b879f4dd1c
Alex Zero <joseph@marsden.space>
2021-07-12 02:13:23 +0100

    feat: add adb/wireshark, update nix

--- profiles/roles/dev/default.nix
+++ profiles/roles/dev/default.nix
@@ -6,9 +6,12 @@
     # enable kernel debugging disabled in security hardening profile
     boot.kernel.sysctl."kernel.ftrace_enabled" = true;
 
-    # wireshark must be enabled here, should it really be though?
-    programs.wireshark.enable = true;
-
     # enable rabbitmq on localhost for various purposes
     services.rabbitmq.enable = true;
+
+    # setcap wrappers and stuff
+    programs = {
+        adb.enable = true;
+        wireshark.enable = true;
+    };
 }
