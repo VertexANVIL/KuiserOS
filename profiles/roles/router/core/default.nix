f29f4c2870a4087bbb34214e1acc160f223a0d4c
Alex Zero <joseph@marsden.space>
2021-12-20 18:17:09 +0000

    fix: override ip forwarding

--- profiles/roles/router/core/default.nix
+++ profiles/roles/router/core/default.nix
@@ -2,13 +2,15 @@
 
 # Useful shared base configuration for NixOS routers
 let
+    inherit (lib) mkForce;
+
     python-custom-pkgs = python-packages: with python-packages; [ pyroute2 ];
     python-custom = with pkgs; python3.withPackages python-custom-pkgs;
 in
 {
     boot.kernel.sysctl = {
-        "net.ipv4.ip_forward" = 1;
-        "net.ipv6.conf.all.forwarding" = 1;
+        "net.ipv4.ip_forward" = mkForce 1;
+        "net.ipv6.conf.all.forwarding" = mkForce 1;
     };
 
     networking = {
