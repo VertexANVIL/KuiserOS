35733ebb5b0930ab5fd68127443c9b97c992381f
Alex Zero <joseph@marsden.space>
2022-04-01 23:15:23 +0100

    feat: make usbguard rules even more specific

--- profiles/hardware/system/p72/default.nix
+++ profiles/hardware/system/p72/default.nix
@@ -19,11 +19,11 @@ in mkProfile {
         hardware.bolt.enable = true;
 
         usbguard.rules = ''
-            allow id 058f:9540 name "EMV Smartcard Reader" with-connect-type "not used"
-            allow id 04f2:b604 name "Integrated Camera" with-connect-type "not used"
-            allow id 04f2:b605 name "Integrated IR Camera" with-connect-type "not used"
-            allow id 8087:0aaa with-connect-type "not used"
-            allow id 06cb:009a with-connect-type "not used"
+            allow id 058f:9540 name "EMV Smartcard Reader" via-port "1-11" with-interface 0b:00:00 with-connect-type "not used"
+            allow id 04f2:b604 name "Integrated Camera" via-port "1-8" with-interface { 0e:01:00 0e:02:00 0e:02:00 0e:02:00 0e:02:00 0e:02:00 0e:02:00 0e:02:00 0e:02:00 } with-connect-type "not used"
+            allow id 04f2:b605 name "Integrated IR Camera" via-port "1-12" with-interface { 0e:01:01 0e:02:01 0e:02:01 0e:02:01 0e:02:01 0e:02:01 0e:02:01 0e:02:01 0e:02:01 } with-connect-type "not used"
+            allow id 8087:0aaa via-port "1-14" with-interface { e0:01:01 e0:01:01 e0:01:01 e0:01:01 e0:01:01 e0:01:01 e0:01:01 e0:01:01 } with-connect-type "not used"
+            allow id 06cb:009a via-port "1-9" with-interface ff:00:00 with-connect-type "not used"
         '';
     };
 
