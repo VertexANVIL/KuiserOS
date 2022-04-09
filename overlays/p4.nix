fec05ea85116607ee0a45be43e8bc8336b1d7206
Alex Zero <joseph@marsden.space>
2022-04-09 18:20:38 +0100

    feat: add pcscd polkit fix & fix p4v

--- overlays/p4.nix
+++ overlays/p4.nix
@@ -1,8 +1,8 @@
 final: prev: with prev; {
     p4 = prev.p4.overrideAttrs (o: {
         src = fetchurl {
-            url = "https://cdist2.perforce.com/perforce/r20.1/bin.linux26x86_64/helix-core-server.tgz";
-            sha256 = "sha256-NmXWllJk4rK1Z8rxngSOGONo2zoRMLAsUnPialbRP/I=";
+            url = "https://cdist2.perforce.com/perforce/r21.2/bin.linux26x86_64/helix-core-server.tgz";
+            sha256 = "sha256-BKts8Pr2r6J5N4SBLtd+pbWDLtiHl3YAjaeKqtVmYqE=";
         };
     });
 }
--- profiles/core/security/smartcard/default.nix
+++ profiles/core/security/smartcard/default.nix
@@ -12,5 +12,18 @@
         ];
     };
 
+    # allow all regular users to access pcscd cards
+    # (this is required to prevent pcscd from blocking access from docker when the uid is 1000)
+    security.polkit.extraConfig = ''
+        polkit.addRule(function (action, subject) {
+            if ((action.id == "org.debian.pcsc-lite.access_pcsc" ||
+                action.id == "org.debian.pcsc-lite.access_card") &&
+                subject.isInGroup("users"))
+            {
+                return polkit.Result.YES;
+            }
+        });
+    '';
+
     environment.systemPackages = with pkgs; [ opensc ];
 }
