b41a4afa4a3c7c12a614ac6166188d166d7c23fb
Alex Zero <me@alex0.net>
2022-11-04 08:59:10 +0000

    feat: add cuda stuff, disable autoupgrade for now

--- profiles/graphical/default.nix
+++ profiles/graphical/default.nix
@@ -22,6 +22,8 @@
         vaapiIntel
         vaapiVdpau
       ];
+
+      setLdLibraryPath = true;
     };
 
     pulseaudio = {
--- profiles/roles/personal/updates.nix
+++ profiles/roles/personal/updates.nix
@@ -1,9 +1,9 @@
 { config, lib, pkgs, ... }:
 {
   # Configures automatic updates
-  system.autoUpgrade = {
-    enable = true;
-    flake = "git+file:///persist/nixos";
-    flags = [ "--update-input" "nixpkgs" ];
-  };
+  # system.autoUpgrade = {
+  #   enable = true;
+  #   flake = "git+file:///persist/nixos";
+  #   flags = [ "--update-input" "nixpkgs" ];
+  # };
 }
