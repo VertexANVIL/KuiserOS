bde96f5e3da39ac7fe07f5d85914d842ff991c4c
Alex Zero <me@alex0.net>
2023-01-20 06:42:38 +0000

    blah

--- overrides/default.nix
+++ overrides/default.nix
@@ -2,11 +2,10 @@
   modules = [
     "security/vault/keys.nix"
     "services/hardware/hp-ams.nix"
-    "services/networking/firewall.nix"
     "services/networking/fort-validator.nix"
     #"services/networking/tayga.nix"
     "services/security/vault-agent.nix"
-    "system/boot/loader/systemd-boot/systemd-boot.nix"
+    #"system/boot/loader/systemd-boot/systemd-boot.nix"
     "virtualisation/containerd.nix"
   ];
   disabledModules = [ ];
